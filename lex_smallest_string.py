a0829d4299142fc31288b0b12e5982833e5c4968
harshalraj08 <38438694+harshalraj08@users.noreply.github.com>
2018-05-01 00:57:20 +0530

    Create lex_smallest_string.py

---
+++ lex_smallest_string.py
@@ -0,0 +1,16 @@
+#find lexicographically smallest string.
+#The first line of input will contain an integer T, denoting the number of test cases. 
+#Each test case starts with 2 numbers N and K - length of S and the number of minutes Kevin has. Next line contains string S.
+
+for i in range(input()):
+	l=[int(x) for x in raw_input().split()]
+	a=raw_input()
+	res="z"*l[0]
+	if(l[1]>1):
+		res=min(a)
+	else:
+		for j in range(0,l[0]):
+			res=min(res,a[j:l[0]])
+		for k in range(1,l[0]+1):
+			res=min(res,a[:k])
+	print(res)
